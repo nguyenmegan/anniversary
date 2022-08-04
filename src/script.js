6ee4b38939f3a631863f10cd7e2f14bbf4912c02
Megan Nguyen <nguyenmegan@berkeley.edu>
2022-08-04 13:00:14 -0700

    scroll features

--- src/script.js
+++ src/script.js
@@ -104,3 +104,27 @@ var reasons = [
   "how much we both love TEA 🍵",
   "hearing you play the piano 🎹",
 ];
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
